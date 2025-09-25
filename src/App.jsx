d5ae5e39f1383a7097eb43547e28936716fcc2f8
Abinaya <141475809+481n4y4@users.noreply.github.com>
2025-09-25 14:27:51 +0700

    ✨ feat(auth): add auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import './App.css'
+import AuthForm from "./component/AuthForm";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase/firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 function App() {
-  return (
-    <></>
-  )
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (currentUser) =>
+      setUser(currentUser)
+    );
+    return () => unsub();
+  }, []);
+
+  return user ? (
+    <div>
+      <h1>Halo {user.email}</h1>
+      <button onClick={() => signOut(auth)}>Logout</button>
+    </div>
+  ) : (
+    <AuthForm />
+  );
 }
 
-export default App
+export default App;
