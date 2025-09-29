f37970b1039487fb92f9a73a80dd58c27e0662ea
Abinaya <141475809+481n4y4@users.noreply.github.com>
2025-09-29 13:57:02 +0700

    📌 chore(auth): auth improvement

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import './assets/css/index.css'
 import Homepage from './page/Homepage'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />          
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Homepage/>}/>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />          
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Homepage/>}/>
+    </Routes>
   );
 }
