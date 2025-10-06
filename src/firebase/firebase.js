988c47ca96fb26618030d611a245d3fab2daa856
Abinaya <141475809+481n4y4@users.noreply.github.com>
2025-10-06 15:11:23 +0700

    📌 chore(api): firebase improvement

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-<<<<<<< HEAD
-import { getAuth } from "firebase/auth";
-=======
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
->>>>>>> 5506e2f776cbd29b443923290e9ad0c3d6cdae86
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,18 +11,10 @@ const firebaseConfig = {
   projectId: "chatme-c3122",
   storageBucket: "chatme-c3122.firebasestorage.app",
   messagingSenderId: "1082706118611",
-<<<<<<< HEAD
-  appId: "1:1082706118611:web:3ec680a3cb2861b6c99126"
-=======
   appId: "1:1082706118611:web:3ec680a3cb2861b6c99126",
->>>>>>> 5506e2f776cbd29b443923290e9ad0c3d6cdae86
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-<<<<<<< HEAD
-export const auth = getAuth(app);
-=======
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
->>>>>>> 5506e2f776cbd29b443923290e9ad0c3d6cdae86
