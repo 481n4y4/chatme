412332c112b9d751d337cd31d898cd77a9dbc0ca
Abinaya <141475809+481n4y4@users.noreply.github.com>
2025-11-03 13:36:02 +0700

    📌 chore(chore): login fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBWxrWxwHlz12jINNZT63TgkRVt9cAObIk",
@@ -15,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app); // ✅ Ganti ke Firestore
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
